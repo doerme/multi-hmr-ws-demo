7a289ce7257ad012da3cfa8af89cec6517e64489
doerme <398921432@qq.com>
2025-04-09 10:27:05 +0800

    refactor(react-host): 删除react-app项目并集成模块联邦功能
    移除react-app项目及其相关文件，同时在react-host项目中添加模块联邦支持，实现远程组件的动态加载。这简化了项目结构并增强了模块化能力。

--- projects/react-host/src/App.tsx
+++ projects/react-host/src/App.tsx
@@ -1,10 +1,32 @@
 import './App.css';
+import { lazy, Suspense } from 'react';
+import { init, loadRemote } from '@module-federation/enhanced/runtime';
+
+init({
+  name: '@demo/app-main',
+  remotes: [
+    {
+      name: "remote",
+      entry: "http://localhost:3002/remoteEntry.js",
+      alias: "remote"
+    }
+  ]
+});
+
+const Button = lazy(() => loadRemote("remote/Button") as any);
 
 const App = () => {
+
+
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <Suspense>
+        <Button />
+      </Suspense>
+      </div>
     </div>
   );
 };
--- projects/react-host/src/Button.tsx
+++ projects/react-host/src/Button.tsx
@@ -1,6 +1,6 @@
 export const Button = () => {
   return (
-    <button style={{ padding: '8px 16px', backgroundColor: '#007bff', color: 'white', border: 'none', borderRadius: '4px' }}>
+    <button style={{ padding: '8px 16px', backgroundColor: '#007bff', color: 'white', border: 'none', borderRadius: '4px', width: '300px' }}>
       Remote Button
     </button>
   );
